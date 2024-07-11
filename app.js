343e40eb15ce95ca5929805c6c1e0290eb3f9d83
piyushkhurana2019 <piyushkhurana159@gmail.com>
2024-07-11 20:04:15 +0530

    Added Middleware for 3rd party Requests

--- app.js
+++ app.js
@@ -11,53 +11,76 @@ app.use(express.json());
 let prev = [],
   curr = [],
   numsReceived = [],
-  result = 0;
+  result = 0,
+  windowSize = 5;
 
-const updateNums = () => {
-  
-}
+const updateNums = async (endpoint) => {
+  numsReceived = await fetch(`http://20.244.56.144/test/${endpoint}`);
+  prev = curr;
+  curr = curr.concat(numsReceived);
+  curr = curr.slice(-windowSize);
+};
 
-app.get("/e", (req, res) => {
-  console.log("Hello World!")
-  return res.status(200).json({
-    "number": numsReceived,
-    "windowPrevState": prev,
-    "windowCurrState": curr,
-    "avg": result
-  });
-})
-
-app.get("/p", (req, res) => {
-  console.log("Hello World!");
+app.get(
+  "/e",
+  () => updateNums("even"),
+  (req, res) => {
+    console.log("Hello World!");
     return res.status(200).json({
       number: numsReceived,
       windowPrevState: prev,
       windowCurrState: curr,
       avg: result,
     });
-});
+  }
+);
 
-app.get("/f", (req, res) => {
-  console.log("Hello World!");
+app.get(
+  "/p",
+  () => updateNums("primes"),
+  (req, res) => {
+    console.log("Hello World!");
     return res.status(200).json({
       number: numsReceived,
       windowPrevState: prev,
       windowCurrState: curr,
       avg: result,
     });
-});
+  }
+);
 
-app.get("/r", (req, res) => {
-  console.log("Hello World!");
+app.get(
+  "/f",
+  () => updateNums("fibo"),
+  (req, res) => {
+    console.log("Hello World!");
+    return res.status(200).json({
+      number: numsReceived,
+      windowPrevState: prev,
+      windowCurrState: curr,
+      avg: result,
+    });
+  }
+);
+
+app.get(
+  "/r",
+  () => updateNums("rand"),
+  (req, res) => {
+    console.log("Hello World!");
     res.status(200).json({
       number: numsReceived,
       windowPrevState: prev,
       windowCurrState: curr,
       avg: result,
     });
-});
+  }
+);
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is listnening on por̉̉t ${process.env.PORT}!`);
 }
 );
+
+const resp = await fetch(`http://20.244.56.144/test/primes`);
+console.log(await resp.json());
