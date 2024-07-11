ca61cb3801352345048f77bbc52a64837266e7f7
piyushkhurana2019 <piyushkhurana159@gmail.com>
2024-07-11 19:56:31 +0530

    Initialized Requests

--- app.js
+++ app.js
@@ -8,10 +8,55 @@ app.use(cors());
 dotenv.config();
 app.use(express.json());
 
+let prev = [],
+  curr = [],
+  numsReceived = [],
+  result = 0;
 
-app.get('/', (req, res) => {  
-    console.log("Process Begin");
+const updateNums = () => {
+  
+}
+
+app.get("/e", (req, res) => {
+  console.log("Hello World!")
+  return res.status(200).json({
+    "number": numsReceived,
+    "windowPrevState": prev,
+    "windowCurrState": curr,
+    "avg": result
+  });
 })
+
+app.get("/p", (req, res) => {
+  console.log("Hello World!");
+    return res.status(200).json({
+      number: numsReceived,
+      windowPrevState: prev,
+      windowCurrState: curr,
+      avg: result,
+    });
+});
+
+app.get("/f", (req, res) => {
+  console.log("Hello World!");
+    return res.status(200).json({
+      number: numsReceived,
+      windowPrevState: prev,
+      windowCurrState: curr,
+      avg: result,
+    });
+});
+
+app.get("/r", (req, res) => {
+  console.log("Hello World!");
+    res.status(200).json({
+      number: numsReceived,
+      windowPrevState: prev,
+      windowCurrState: curr,
+      avg: result,
+    });
+});
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is listnening on por̉̉t ${process.env.PORT}!`);
 }
